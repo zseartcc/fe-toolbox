208a766be668677fcd438b34d1f6c478e6507855
Teddy <63885870+cessnahat@users.noreply.github.com>
2021-01-17 10:33:53 -0800

    Add beginnings of fetools library

---
+++ fetools/README.md
@@ -0,0 +1,24 @@
+# FE Tools
+
+A toolset for VATUSA FEs.
+
+## Requirements
+- Python 3
+
+
+## Installation
+Run `pip install fetools` from the terminal (coming soon)
+
+
+## Documentation
+Coming soon.
+
+
+## License
+[GNU GPLv3](https://www.gnu.org/licenses/gpl-3.0.en.html)
+
+
+## Changelog
+
+#### `1.0.0` (17 Jan 2021)
+- Misc. helper functions
---
+++ fetools/fetools/__init__.py
@@ -0,0 +1,29 @@
+import os
+
+__all__ = ["pause", "clear", "VRC", "VSTARS", "VERAM"]
+
+
+#########################
+# Lib-level constants
+#########################
+
+VRC = "vrc"
+VSTARS = "veram"
+VERAM = "veram"
+
+
+#########################
+# Misc. helpers
+#########################
+
+def pause():
+	""" Does 'Press any key to continue' """
+    if os.name == "nt":
+        print()
+        os.system("pause")
+    else:
+        input("\nPress Enter to continue . . . ")
+
+def clearscreen():
+    """ Clear the terminal/command prompt """
+    os.system("cls" if os.name=="nt" else "clear")
